7f3cdb17323d04bf98a25fac22e735f9c2a67ebd
taek <leekt216@gmail.com>
2019-08-20 06:33:32 +0000

    Initial commit

---
+++ truffle-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // eslint-disable-line camelcase
+    },
+    coverage: {
+      host: 'localhost',
+      network_id: '*', // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01
+    },
+    ganache: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*' // eslint-disable-line camelcase
+    },
+    ...require('./scripts/local_eth_ganache_option.js')
+  },
+  compilers: {
+    ...require('./solc.json')
+  },
+  mocha: {
+    reporter: 'eth-gas-reporter',
+    reporterOptions: {
+      currency: 'KRW',
+      gasPrice: 5
+    }
+  }
+};
